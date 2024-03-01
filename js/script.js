ab592f9a1a6b356818bc81d734b38aeeb92a7c2d
Francesco <fotifgerend@gmail.com>
2024-03-01 17:50:32 +0100

    Aggiunta bottoni scorrimento

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+function leftScroll() {
+    const buttonLeft = document.querySelector(".menu-cards-novita");
+    buttonLeft.scrollBy(200, 0);
+  }
+
+  function rightScroll() {
+    const buttonRight = document.querySelector(".menu-cards-novita");
+    buttonRight.scrollBy(-200, 0);
+  }
