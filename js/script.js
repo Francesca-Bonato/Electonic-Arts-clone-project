7c2623339040ec8bcbc8286493bdc3d8f19f9801
Luigi Balestrucci <card3001@outlook.it>
2024-03-01 19:00:43 +0100

    Update scroll-bar menu

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
+let btn = document.querySelector('#menu-novita-scroll-right');
+let btn2 = document.querySelector('#menu-novita-scroll-left');
 
+btn.addEventListener("click", function() {
+  document.querySelector(".menu-scroll-active").scrollLeft += 200;
+  btn2.classList.add('active-arrow')
+});
 
-function leftScroll() {
-    const buttonLeft = document.querySelector(".menu-cards-novita");
-    buttonLeft.scrollBy(200, 0);
-  }
+btn2.addEventListener("click", function() {
+  document.querySelector(".menu-scroll-active").scrollLeft -= 200;
+});
 
-  function rightScroll() {
-    const buttonRight = document.querySelector(".menu-cards-novita");
-    buttonRight.scrollBy(-200, 0);
-  }
+// function leftScroll() {
+//     const buttonLeft = document.querySelector(".menu-cards-novita");
+//     buttonLeft.scrollBy(200, 0);
+//   }
+
+//   function rightScroll() {
+//     const buttonRight = document.querySelector(".menu-cards-novita");
+//     buttonRight.scrollBy(-200, 0);
+//   }
