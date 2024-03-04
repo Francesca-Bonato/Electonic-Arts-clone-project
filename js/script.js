28345392351fb37f599d5e99f9bec13b74b73612
Daniele De Lise <danieledelise12@gmail.com>
2024-03-04 17:10:19 +0100

    Aggiunte freccette che scompaiono al click, ancora da risolvere il bug che fa sparire i primi elementi li

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
-let btn = document.querySelector('#menu-novita-scroll-right');
-let btn2 = document.querySelector('#menu-novita-scroll-left');
+document.addEventListener("DOMContentLoaded", function () {
+  const scrollMenu = document.querySelector(".menu-scroll-active");
+  const leftButton = document.querySelector("#menu-novita-scroll-left");
+  const rightButton = document.querySelector("#menu-novita-scroll-right");
 
-btn.addEventListener("click", function() {
-  document.querySelector(".menu-scroll-active").scrollLeft += 200;
-  btn2.classList.add('active-arrow')
+  function checkScroll() {
+    const scrollLength = scrollMenu.scrollWidth - scrollMenu.clientWidth;
+    const currentScroll = scrollMenu.scrollLeft;
+    if (currentScroll === 0) {
+      leftButton.classList.add("hidden");
+      rightButton.classList.remove("hidden");
+    } else if (currentScroll === scrollLength) {
+      rightButton.classList.add("hidden");
+      leftButton.classList.remove("hidden");
+    } else {
+      leftButton.classList.remove("hidden");
+      rightButton.classList.remove("hidden");
+    }
+  }
+
+  scrollMenu.addEventListener("scroll", checkScroll);
+  window.addEventListener("resize", checkScroll);
+  checkScroll();
+
+  function leftScroll() {
+    scrollMenu.scrollBy({
+      left: -200,
+      behavior: "smooth"
+    });
+  }
+
+  function rightScroll() {
+    scrollMenu.scrollBy({
+      left: 200,
+      behavior: "smooth"
+    });
+  }
+
+  leftButton.addEventListener("click", leftScroll);
+  rightButton.addEventListener("click", rightScroll);
 });
 
-btn2.addEventListener("click", function() {
-  document.querySelector(".menu-scroll-active").scrollLeft -= 200;
-});
-
-// function leftScroll() {
-//     const buttonLeft = document.querySelector(".menu-cards-novita");
-//     buttonLeft.scrollBy(200, 0);
-//   }
-
-//   function rightScroll() {
-//     const buttonRight = document.querySelector(".menu-cards-novita");
-//     buttonRight.scrollBy(-200, 0);
-//   }
