64f92e76573344138f4733f43db81c4237cc70af
Francesco <ballerifrancesco@gmail.com>
2024-03-25 08:12:58 +0100

    select on click fix

--- script.js
+++ script.js
@@ -9,28 +9,46 @@ copyrightYear.innerText = currentYear;
 
 
 
-
 const pricingSelect = document.querySelector("#ea-regional-pricing-button")
 const pricingList = document.querySelector("#princing-items")
 
 const localeSelect = document.querySelector("#ea-locale-button")
 const localeList = document.querySelector("#locale-items")
 
+const mediaQuery = window.matchMedia("(min-width: 1007px)")
 
+/* ! Function to toggle div in pricing select on 1007px window*/
 function pricingOnClick() {
-    pricingSelect.addEventListener('click', () => {
-        pricingList.classList.toggle("openPrice")
+    pricingSelect.addEventListener('click', (e) => {
+        if (mediaQuery.matches) {
+            pricingList.classList.toggle("openPrice")
+            e.stopPropagation()
+        } else {
+            pricingList.classList.remove("openPrice")
+        }
+    })
+
+    document.addEventListener("click", (e) => {
+        pricingList.classList.remove("openPrice")
+        e.stopPropagation()
     })
 }
-
 pricingOnClick()
 
-
-
+/* ! Function to toggle div in locale select on 1007px window*/
 function localeOnClick() {
-    localeSelect.addEventListener('click', () => {
-        localeList.classList.toggle("openLocale")
+    localeSelect.addEventListener('click', (e) => {
+        if (mediaQuery.matches) {
+            localeList.classList.toggle("openPrice")
+            e.stopPropagation()
+        } else {
+            localeList.classList.remove("openPrice")
+        }
+    })
+
+    document.addEventListener("click", (e) => {
+        localeList.classList.remove("openPrice")
+        e.stopPropagation()
     })
 }
-
 localeOnClick()
