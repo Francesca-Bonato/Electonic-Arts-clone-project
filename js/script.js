6897edd0716bec49e90aa8e713a6b1e28a2bc90c
Luigi Balestrucci <card3001@outlook.it>
2024-03-25 21:26:14 +0100

    Navbar completed

--- js/script.js
+++ js/script.js
@@ -156,128 +156,68 @@ subGamesFive.addEventListener('mouseout', () => {
   document.body.classList.remove('open-resources');
 })
 
-// Animazione scroll navbar
-let content = document.querySelector(".content");
-let subMenu = document.querySelector(".container-section-games");
-let menu = document.querySelector(".games");
+/*  -----------------------------------------------------------------------------------------------
+  MENU RESPONSIVE
+--------------------------------------------------------------------------------------------------- */
+//Take a single menu and element to close sub-menu:
+const resourceResponsive = document.querySelector('.resources-responsive');
+const engagementResponsive = document.querySelector('.engagements-responsive');
+const informationResponsive = document.querySelector('.information-responsive');
+const experienceResponsive = document.querySelector('.experience-responsive');
+const gamesResponsive = document.querySelector('.games-responsive .container-section-menu-responsive');
+const exploreGames = document.querySelector('.explore_games_list-responsive');
+const platformList = document.querySelector('.platform-responsive');
+const hamburgherIcon = document.querySelector('.icon-hamburgher')
+const backgroundResponsive = document.querySelector('.background-responsive-menu');
+const closerElementResponsive = document.querySelector('.header-responsive-menu span');
 
-document.addEventListener("scroll", () => {
-  if ((lastKnownScrollPosition = window.scrollY)) {
-    content.classList.add("active");
-  } else {
-    content.classList.remove("active");
-  }
-});
+//Open menu responsive on click:
+hamburgherIcon.addEventListener('click', () => {
+  document.body.classList.toggle('open')
+})
+//Add event to open sub-menu on click:
+resourceResponsive.addEventListener('click', () => {
+  document.body.classList.toggle('opened-responsive-resource');
+})
 
-// Animazione nav a comparsa laterale
-let close_lateral_menu = document.querySelector(".close_icon");
-let lateral_nav = document.querySelector(".container-nav-laterale");
-let openLatealMenu = document.querySelector(".open_icon");
-let lateralNav = document.querySelector(".nav-laterale");
+engagementResponsive.addEventListener('click', () => {
+  document.body.classList.toggle('opened-responsive-engagement');
+})
 
-openLatealMenu.addEventListener("click", (e) => {
-  e.preventDefault();
-  document.body.classList.add("open");
-});
+informationResponsive.addEventListener('click', () => {
+  document.body.classList.toggle('opened-responsive-information');
+})
 
-close_lateral_menu.addEventListener("click", () => {
-  document.body.classList.remove("open");
-});
+experienceResponsive.addEventListener('click', () => {
+  document.body.classList.toggle('opened-responsive-experience');
+})
 
-lateralNav.addEventListener("click", () => {
-  document.body.classList.remove("open");
-});
+/*  -----------------------------------------------------------------------------------------------
+  GAMES SUBSECTION EVENT
+--------------------------------------------------------------------------------------------------- */
+gamesResponsive.addEventListener('click', () => {
+  document.body.classList.toggle('opened-responsive-games');
+  document.body.classList.remove('open-list-games')
+  document.body.classList.remove('open-list-platform')
+})
 
-// Animazione sub menu nav uno
-const close_user_profile = document.querySelector(".close_user_profile");
-const sub_container_user_profile = document.querySelector(
-  ".sub_container_user_profile"
-);
-const sub_container_help_profile = document.querySelector(".second");
-const open_user_profile = document.querySelector(".user_profile");
-const open_help = document.querySelector(".help");
-const close_help = document.querySelector(".close_help");
+exploreGames.addEventListener('click', () => {
+  document.body.classList.toggle('open-list-games');
+})
 
-open_user_profile.addEventListener("click", (e) => {
-  e.preventDefault();
-  sub_container_user_profile.classList.toggle("open_user_profile");
-  sub_container_help_profile.classList.remove("open_user_profile");
-});
+platformList.addEventListener('click', () => {
+  document.body.classList.toggle('open-list-platform');
+})
 
-open_help.addEventListener("click", (e) => {
-  e.preventDefault();
-  sub_container_help_profile.classList.toggle("open_user_profile");
-  sub_container_user_profile.classList.remove("open_user_profile");
-});
+//Close all menu on click closer or background container:
+backgroundResponsive.addEventListener('click', () => {
+  document.body.classList.remove('open')
+  document.body.classList.remove('overflow-disabled-info')
+  document.body.classList.remove('opened-info-superior')
+  document.body.classList.remove('overflow-disabled-user')
+  document.body.classList.remove('opened-user-superior')
+})
 
-close_user_profile.addEventListener("click", () => {
-  sub_container_user_profile.classList.remove("open_user_profile");
-});
-
-close_help.addEventListener("click", () => {
-  sub_container_help_profile.classList.remove("open_user_profile");
-});
-
-// Animazione menu laterale responsive
-const another_experience_responsive = document.querySelector(
-  ".another_experience_responsive"
-);
-const information_responsive = document.querySelector(
-  ".information_responsive"
-);
-const engagements_responsive = document.querySelector(
-  ".engagements_responsive"
-);
-const resources_responsive = document.querySelector(".resources_responsive");
-const subMenuGames = document.querySelector(".games_responsive");
-const subGames = document.querySelector(".explore_games");
-const subPlatform = document.querySelector(".review_platform");
-const close_responsive = document.querySelector(".close_responsive");
-const icon_hamburger_menu = document.querySelector(".icon_hamburger_menu");
-const container_lateral_menu_responsive = document.querySelector(
-  ".container-lateral-responsive-menu"
-);
-
-another_experience_responsive.addEventListener("click", () => {
-  document.body.classList.toggle("opened");
-});
-
-information_responsive.addEventListener("click", () => {
-  document.body.classList.toggle("opened_information");
-});
-
-engagements_responsive.addEventListener("click", () => {
-  document.body.classList.toggle("opened_engagements");
-});
-
-resources_responsive.addEventListener("click", () => {
-  document.body.classList.toggle("opened_resources");
-});
-
-subMenuGames.addEventListener("click", () => {
-  document.body.classList.toggle("opened_games");
-  document.body.classList.remove("opened_sub_platform");
-  document.body.classList.remove("opened_sub_games");
-});
-
-subGames.addEventListener("click", () => {
-  document.body.classList.toggle("opened_sub_games");
-});
-
-subPlatform.addEventListener("click", () => {
-  document.body.classList.toggle("opened_sub_platform");
-});
-
-icon_hamburger_menu.addEventListener("click", () => {
-  document.body.classList.add("open_lateral_menu_responsive");
-});
-
-close_responsive.addEventListener("click", () => {
-  document.body.classList.remove("open_lateral_menu_responsive");
-});
-
-container_lateral_menu_responsive.addEventListener("click", () => {
-  document.body.classList.remove("open_lateral_menu_responsive");
-});
-
-// Animazione 
+closerElementResponsive.addEventListener('click', () => {
+  document.body.classList.remove('open')
+})
