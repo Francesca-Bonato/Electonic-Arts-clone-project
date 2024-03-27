a086d9cb00e7499b163101032b5861de650e3683
Francesca Bonato <francesca.bonato@hotmail.com>
2024-03-28 00:34:00 +0100

    various style fixes - hover effects and font sizes - + js to change the language and price

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
-// Set the copyright dynamically in the footer
-const copyrightYear = document.querySelector("#copyright-year");
+//access the two select elements (active below 1007px)
+let pricingSelect = document.querySelector('#network-footer-pricing');
+let localeSelect = document.querySelector('#network-footer-language');
 
-// Get the current year
-const currentYear = new Date().getFullYear();
+//access the span that will show the selected pricing area
+const selectedPricingSpan = document.querySelector('.ea-regional-pricing-button-selected-text')
+selectedPricingSpan.innerText = pricingSelect.value;
 
-// Update the content of the element with the ID "copyright-year"
-copyrightYear.innerText = currentYear;
+//add an event listener for the "change" event of the "select" element
+pricingSelect.addEventListener('change', () => {
+    const selectedPricing = pricingSelect.value;
+    selectedPricingSpan.innerText=selectedPricing;
+})
+
+//access the span that will show the selected locale area and the one for the corresponding flag
+const selectedLocaleSpan = document.querySelector('.ea-locale-selector__selected-text')
+selectedLocaleSpan.innerText = localeSelect.value;
+
+const selectedLocaleFlagSpan = document.querySelector('.ea-locale-selector__selected-flag')
+selectedLocaleFlagSpan.id = (localeSelect.value).split(" ").join("-");
+
+//add an event listener for the "change" event of the "select" element
+localeSelect.addEventListener('change', () => {
+    const selectedLocale = localeSelect.value;
+    selectedLocaleSpan.innerText=selectedLocale;
+    selectedLocaleFlagSpan.id = (localeSelect.value).split(" ").join("-");
+})
 
 
 
-const pricingSelect = document.querySelector("#ea-regional-pricing-button")
+//access the two network footer buttons relative to pricing and locale:
+const pricingButton = document.querySelector("#ea-regional-pricing-button")
+const localeButton = document.querySelector("#ea-locale-button")
+
+//access the two div elements (active above 1007px)
 const pricingList = document.querySelector("#princing-items")
-
-const localeSelect = document.querySelector("#ea-locale-button")
 const localeList = document.querySelector("#locale-items")
 
 const mediaQuery = window.matchMedia("(min-width: 1007px)")
@@ -23,7 +44,7 @@ const localeClassToHide = document.querySelector(".openLocale")
 // ! Function to toggle div in pricing select on 1007px window
 function pricingOnClick() {
     // ! if click select add div class 
-    pricingSelect.addEventListener('click', (e) => {
+    pricingButton.addEventListener('click', (e) => {
         if (mediaQuery.matches) {
             pricingList.classList.toggle("openPrice")
             e.stopPropagation()
@@ -41,12 +62,10 @@ function pricingOnClick() {
 
 pricingOnClick()
 
-
-
 // ! Function to toggle div in locale select on 1007px window
 function localeOnClick() {
     // ! if click select add div class 
-    localeSelect.addEventListener('click', (e) => {
+    localeButton.addEventListener('click', (e) => {
         if (mediaQuery.matches) {
             localeList.classList.toggle("openLocale")
             e.stopPropagation()
@@ -63,3 +82,16 @@ function localeOnClick() {
 }
 
 localeOnClick()
+
+//
+
+//---- Copyright
+
+// Set the copyright dynamically in the footer
+const copyrightYear = document.querySelector("#copyright-year");
+
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Update the content of the element with the ID "copyright-year"
+copyrightYear.innerText = currentYear;
